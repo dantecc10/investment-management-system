68beada328a8f9877f2c967c72443d82d98963ec
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-03-14 15:50:23 -0600

    typo

--- php scripts/gantt/core/controller/Database.php
+++ php scripts/gantt/core/controller/Database.php
@@ -1,23 +1,28 @@
 <?php
-class Database {
+class Database
+{
 	public static $db;
 	public static $con;
-	function __construct(){
-		$this->user="ims_admin";$this->pass="AdminIMS24!!";$this->host="localhost";$this->ddbb="ims_system";
+	function __construct()
+	{
+		$this->user = "ims_admin";
+		$this->pass = "AdminIMS24!!";
+		$this->host = "localhost";
+		$this->ddbb = "ims-system";
 	}
 
-	function connect(){
-		$con = new mysqli($this->host,$this->user,$this->pass,$this->ddbb);
+	function connect()
+	{
+		$con = new mysqli($this->host, $this->user, $this->pass, $this->ddbb);
 		return $con;
 	}
 
-	public static function getCon(){
-		if(self::$con==null && self::$db==null){
+	public static function getCon()
+	{
+		if (self::$con == null && self::$db == null) {
 			self::$db = new Database();
 			self::$con = self::$db->connect();
 		}
 		return self::$con;
 	}
-	
 }
-?>
